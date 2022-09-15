834c24ec2d4750c424707383c41bd51bd6fa732e
sl243 <slmiucse@gmail.com>
2022-09-15 19:52:25 +0600

    type mistake count added

--- script.js
+++ script.js
@@ -47,6 +47,7 @@ const typeController = (e) => {
     display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
   } else {
     display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
+    errorCount++;
   }
 
   // check if given question text is equal to user typed text
